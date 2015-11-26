1c5730c8fcad6e978203051ba94442bd55ad4947
root <root@localhost.localdomain>
2015-11-26 21:37:38 +0800

    component

--- client/app/appGrid.js
+++ client/app/appGrid.js
@@ -1,3 +1,53 @@
 /**
  * Created by bai on 2015/11/24.
  */
+/**
+ * Created by bai on 2015/11/24.
+ */
+/**
+ * Created by bai on 2015/11/24.
+ */
+Template.appGrid.helpers({
+    name: function () {
+        return 'name';
+    },
+    value: function () {
+        return 'value';
+    },
+    unit: function () {
+        return 'unit';
+    }
+});
+
+Template.appGrid.events({});
+
+Template.appGrid.onRendered(function () {
+
+        $('.editable').editable({
+            success: function (response, newValue) {
+                //<do something with newValue - usually a collection.update call>
+            }
+        });
+        $('.edit-select').editable({
+            value: 2,
+            source: [
+                {value: 1, text: 'Active'},
+                {value: 2, text: 'Blocked'},
+                {value: 3, text: 'Deleted'}
+            ]
+        });
+        $('.status').editable({
+            value: 2,
+            source: [
+                {value: 1, text: 'Active'},
+                {value: 2, text: 'Blocked'},
+                {value: 3, text: 'Deleted'}
+            ]
+        });
+    }
+);
+
+Template.appGrid.onCreated(function () {
+        //$.fn.editable.defaults.mode = 'inline';
+    }
+);
--- client/app/appTree.js
+++ client/app/appTree.js
@@ -1,3 +1,57 @@
 /**
  * Created by bai on 2015/11/24.
  */
+/**
+ * Created by bai on 2015/11/24.
+ */
+Template.appTree.helpers({});
+
+Template.appTree.events({});
+
+Template.appTree.onRendered(function () {
+        function getTree() {
+            // Some logic to retrieve, or generate tree structure
+            var tree = [
+                {
+                    text: "Parent 1",
+                    nodes: [
+                        {
+                            text: "Child 1",
+                            nodes: [
+                                {
+                                    text: "Grandchild 1"
+                                },
+                                {
+                                    text: "Grandchild 2"
+                                }
+                            ]
+                        },
+                        {
+                            text: "Child 2"
+                        }
+                    ]
+                },
+                {
+                    text: "Parent 2"
+                },
+                {
+                    text: "Parent 3"
+                },
+                {
+                    text: "Parent 4"
+                },
+                {
+                    text: "Parent 5"
+                }
+            ];
+            return tree;
+        }
+
+        $('#tree').treeview({data: getTree()});
+    }
+);
+
+Template.appTree.onCreated(function () {
+
+    }
+);
